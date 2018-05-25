4c4d8cc9e6ef20956f87582089039bffa0daea7a
alokinfosec <alokdixit.iitkgp@gmail.com>
2018-05-25 12:20:05 +0530

    added RMSE

--- Backprop.py
+++ Backprop.py
@@ -18,6 +18,9 @@ def elem_wise_multiply_and_add(input, weights, bias):
 def apply_activation_function(output, func):
     return [func(output_i) for output_i in output]
 
+def RMSE(output, labels):
+    return sum([((out-label)**2)/2 for out, label in zip(output, labels)])**0.5
+
 INPUT_DIM = 2
 num_of_hidden_layers = 2
 OUTPUT_DIM = 1
@@ -35,15 +38,15 @@ def RELU(input):
 activation_function = RELU
 
 layers = []
-layers.append(list({"w":1, "b":0, "neuron_id":"input_{}".format(i)} for i in range((INPUT_DIM))))
+layers.append(list({"w":1, "b":0.1, "neuron_id":"input_{}".format(i)} for i in range((INPUT_DIM))))
 
 for idx, num_of_hidden_neuron in enumerate(num_of_neurons_hidden_layer):
     last_layer_neurons = INPUT_DIM if idx == 0 else num_of_neurons_hidden_layer[idx-1]
-    layers.append([{"w":np.random.rand(last_layer_neurons), "b":0, "neuron_id":"hidden{}_{}".format(idx, i)} for i in range(num_of_hidden_neuron)])
+    layers.append([{"w":np.random.rand(last_layer_neurons), "b":0.1, "neuron_id":"hidden{}_{}".format(idx, i)} for i in range(num_of_hidden_neuron)])
 
 
 
-layers.append(list({"w":np.random.rand(num_of_neurons_hidden_layer[-1]), "b":0, "neuron_id":"output_{}".format(i)} for i in range(OUTPUT_DIM)))
+layers.append(list({"w":np.random.rand(num_of_neurons_hidden_layer[-1]), "b":0.1, "neuron_id":"output_{}".format(i)} for i in range(OUTPUT_DIM)))
 
 
 df = pd.read_csv("DATA.csv")
@@ -55,7 +58,7 @@ label = df["z"]
 
 for x, y in zip(input_x,input_y):
     h1_input = (x*layers[0][0]["w"], y*layers[0][1]["w"])
-    print("output of layer_0 is {} and will act as inputs to hidden layer 1".format(str(h1_input)))
+    # print("output of layer_0 is {} and will act as inputs to hidden layer 1".format(str(h1_input)))
     
     layer_i_input = h1_input
     for layer_i in layers[1:-1]:
@@ -72,6 +75,10 @@ for x, y in zip(input_x,input_y):
 
     print("final output for input = ({0}, {1}) is {2}".format(x, y, str(output)))
 
+    # Now we will calculate the error and update the weights
+
+    err = RMSE(output, label)
+    print(err)
 
 
 
